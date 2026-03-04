86ede03e8a70e70e87b249c2adbd96a043afa6ab
pagannnnn <diegoserr091111@gmail.com>
2026-03-04 16:43:39 -0600

    OperacionesLogicas

---
+++ OperacionesLogicas.js
@@ -0,0 +1,14 @@
+
+
+let a = true;
+let b = false;
+
+// AND
+console.log("a AND b:", a && b);
+
+// OR
+console.log("a OR b:", a || b);
+
+// NOT
+console.log("NOT a:", !a);
+console.log("NOT b:", !b);
