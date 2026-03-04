a90084f29603e6783cc594dc74a1b5698d8f760d
pagannnnn <diegoserr091111@gmail.com>
2026-03-04 16:38:25 -0600

    OperadoresBasicos

---
+++ OperadoresBasicos.js
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <title>Operaciones básicas</title>
+</head>
+<body>
+  <h1>Operaciones básicas</h1>
+  <script src="script.js"></script>
+</body>
+</html>
+
+//JAVA SCRIPT
+const a = 10;
+const b = 5;
+
+console.log("Suma:", a + b);
+console.log("Resta:", a - b);
+console.log("Multiplicación:", a * b);
+console.log("División:", a / b);
