4a65b7e2f284b797c19d288c057d2bc055d51d0f
pagannnnn <diegoserr091111@gmail.com>
2026-03-04 16:41:09 -0600

    ComparaNumeros

---
+++ ComparaNumeros.js
@@ -0,0 +1,12 @@
+
+
+let numero1 = 10;
+let numero2 = 20;
+
+if (numero1 > numero2) {
+    console.log("El número mayor es: " + numero1);
+} else if (numero2 > numero1) {
+    console.log("El número mayor es: " + numero2);
+} else {
+    console.log("Ambos números son iguales");
+}
